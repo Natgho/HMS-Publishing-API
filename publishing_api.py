ec5b26de70038da5df561c1693e74ec0733206f5
Sezer BOZKIR <sezer.bozkir@huawei.com>
2020-06-29 17:23:14 +0300

    upload_app and upload_app_language endpoints implemented. Some minor improvements.

--- publishing_api.py
+++ publishing_api.py
@@ -1,7 +1,7 @@
 # Created by SezerBozkir<admin@sezerbozkir.com> at 6/26/2020
 import requests
 from http import HTTPStatus
-from pub_env import *
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class HmsPublishingApi:
@@ -19,6 +19,10 @@ class HmsPublishingApi:
         self.package_name = package_name
 
         self.access_token = self.get_access_token()
+        self.header = {
+            'client_id': self.client_id,
+            'Authorization': 'Bearer ' + self.access_token,
+        }
         self.app_id = self.get_app_id()
 
     def get_access_token(self):
@@ -41,7 +45,7 @@ class HmsPublishingApi:
             try:
                 print(f"Access Token: {creds['access_token']}\n valid: {creds['expires_in']}")
                 self.access_token = creds['access_token']
-            except Exception as e:
+            except:
                 print(resp.json().get('ret').get('msg'))
         else:
             print(f"Error: {resp.status_code} {resp.reason}")
@@ -56,17 +60,12 @@ class HmsPublishingApi:
         if self.app_id:
             return self.app_id
 
-        header = {
-            'client_id': self.client_id,
-            'Authorization': 'Bearer ' + self.access_token,
-        }
-
         params = {
             'client_id': self.client_id,
             'Authorization': 'Bearer ' + self.access_token,
             'packageName': self.package_name
         }
-        resp = requests.get(self.base_api + "/publish/v2/appid-list", headers=header, params=params)
+        resp = requests.get(self.base_api + "/publish/v2/appid-list", headers=self.header, params=params)
         if resp.status_code == HTTPStatus.OK:
             print(f"App name: {resp.json()['appids'][0].get('key')}, App ID: {resp.json()['appids'][0].get('value')}")
             self.app_id = resp.json()['appids'][0].get('value')
@@ -74,16 +73,16 @@ class HmsPublishingApi:
             print(f"Error: {resp.status_code} {resp.reason}")
         return self.app_id
 
-    def get_upload_url(self, upload_file_type='apk'):
+    def get_upload_url(self, file_type='apk'):
         if self.upload_url:
             return self.upload_url
 
         params = {
             'appId': self.app_id,
-            'suffix': upload_file_type
+            'suffix': file_type
         }
         header = {
-            'client_id': client_id,
+            'client_id': self.client_id,
             'Authorization': 'Bearer ' + self.access_token
         }
         resp = requests.get(self.base_api + "/publish/v2/upload-url", params=params, headers=header)
@@ -94,29 +93,42 @@ class HmsPublishingApi:
             self.auth_code = creds['authCode']
         return self.upload_url
 
-    def upload_app(self, file_name):
+    def upload_app(self, file_path, file_name, file_type):
         if not self.upload_url:
-            self.upload_url = self.get_upload_url()
+            self.upload_url = self.get_upload_url(file_type=file_type)
 
         header = {
-            'client_id': self.client_id,
-            'Authorization': 'Bearer ' + self.access_token,
-            "Content-Type": "application/json"
+            "accept": "application/json"
         }
 
         req_body = {
             'authCode': self.auth_code,
-            'fileCount': 1,
-            'name': file_name
+            'fileCount': 1
         }
-        #  + "/publish/v2/app-package-file"
-        with open('sample-apk.apk', 'rb') as f:
-            resp = requests.post(self.upload_url,
-                                 files={'file': f},
-                                 data=req_body,
-                                 params={'appid': self.app_id},
-                                 headers=header)
-        return resp
+        with open(file_path, 'rb') as f:
+            first_phase = requests.post(self.upload_url,
+                                        files={file_name: f},
+                                        data=req_body,
+                                        headers=header)
+            if first_phase.status_code == HTTPStatus.OK:
+                body = {
+                    'fileType': 5,
+                    'lang': 'tr-TR',
+                    'files': [{
+                        'fileName': file_name,
+                        'fileDestUrl': first_phase.json()['result']['UploadFileRsp']['fileInfoList'][0]['fileDestUlr'],
+                        'size': str(first_phase.json()['result']['UploadFileRsp']['fileInfoList'][0]['size'])
+                    }]
+                }
+                params = {
+                    'appId': self.app_id
+                }
+                second_phase = requests.put(self.base_api + "/publish/v2/app-file-info",
+                                            headers=self.header,
+                                            json=body,
+                                            params=params)
+
+        return second_phase
 
     def get_app_info(self):
         """
@@ -125,15 +137,10 @@ class HmsPublishingApi:
         :return:
         """
 
-        header = {
-            'client_id': self.client_id,
-            'Authorization': 'Bearer ' + self.access_token,
-        }
-
         params = {
             'appId': self.app_id,
         }
-        resp = requests.get(self.base_api + "/publish/v2/app-info", headers=header, params=params)
+        resp = requests.get(self.base_api + "/publish/v2/app-info", headers=self.header, params=params)
         return resp.json()
 
     def update_app_info(self, update_params: dict):
@@ -144,10 +151,6 @@ class HmsPublishingApi:
         :param update_params: key value pairs you want to update
         :return:
         """
-        header = {
-            'client_id': self.client_id,
-            'Authorization': 'Bearer ' + self.access_token,
-        }
         params = {
             'appId': self.app_id,
         }
@@ -155,12 +158,41 @@ class HmsPublishingApi:
         for key, value in update_params.items():
             body[key] = value
 
-        resp = requests.put(self.base_api + "/publish/v2/app-info", headers=header, data=body, params=params)
+        resp = requests.put(self.base_api + "/publish/v2/app-info", headers=self.header, data=body, params=params)
         if resp.status_code == HTTPStatus.OK:
             return resp.json()
         return resp
 
+    def add_app_language(self, lang, app_name, app_desc=None, brief_info=None, new_features=None):
+        """
+        appName = App name in a language. This parameter is mandatory when a language type is added.
+        appDesc = App description in a language.
+        briefInfo = Brief description in a language.
+        newFeatures = Introduction to the new version in a language.
 
-if __name__ == '__main__':
-    my_hms_api = HmsPublishingApi(client_id, client_secret, package_name)
-    my_hms_api.get_app_id()
+        This API is used to update multi-language information about a specified app.
+        If no multi-language information is available, this API is used to add the multi-language information.
+        Callers of this API include account holders, administrators, app administrators, and operations personnel.
+        https://developer.huawei.com/consumer/en/doc/development/AppGallery-connect-References/agcapi-app-language-info_update_v2
+        :return:
+        """
+        params = {
+            'appId': self.app_id,
+        }
+        body = {
+            'lang': lang,
+            'appName': app_name
+        }
+
+        if app_desc:
+            body['appDesc'] = app_desc
+        if brief_info:
+            body['briefInfo'] = brief_info
+        if new_features:
+            body['newFeatures'] = new_features
+
+        resp = requests.put(self.base_api + "/publish/v2/app-language-info",
+                            headers=self.header,
+                            data=body,
+                            params=params)
+        return resp
