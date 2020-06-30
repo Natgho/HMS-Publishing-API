9b5300a19c5dfde14893680050c3780864b8b7e5
Sezer BOZKIR <sezer.bozkir@huawei.com>
2020-06-30 15:00:37 +0300

    information lines added

--- examples/UpdateAppInfo.py
+++ examples/UpdateAppInfo.py
@@ -5,10 +5,10 @@ from pub_env import client_id, client_secret, package_name
 if __name__ == '__main__':
     my_api = HmsPublishingApi(client_id, client_secret, package_name)
     before_app_info = my_api.get_app_info()
-    print(before_app_info['appInfo']['defaultLang'])
+    print("Before language: " +  before_app_info['appInfo']['defaultLang'])
     # default lang is sample. if you want to look at all of parameters, you can look at:
     # https://developer.huawei.com/consumer/en/doc/development/AppGallery-connect-References/agcapi-app-info_update_v2
     update_vals = {'defaultLang': 'en-US'}
     my_api.update_app_info(update_vals)
     after_app_info = my_api.get_app_info()
-    print(after_app_info['appInfo']['defaultLang'])
+    print("After Language: " + after_app_info['appInfo']['defaultLang'])
